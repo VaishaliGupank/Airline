5f43afe209684f4dbe72ee1a271f07b9357cc69f
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-09 15:03:58 +0530

    [sandipa/vaishali] user story 4 changes

--- src/main/java/airline/Airline.java
+++ src/main/java/airline/Airline.java
@@ -1,8 +1,12 @@
 package airline;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class Airline {
     public static void main(String []args) {
 
