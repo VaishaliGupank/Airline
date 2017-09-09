39df946ef42e8d4299a8b071ec57d520debdc4c2
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-09 14:59:23 +0530

    [sandipa/vaishali] user story 4 changes

--- src/main/java/airline/ViewModels/FlightSearchViewModel.java
+++ src/main/java/airline/ViewModels/FlightSearchViewModel.java
@@ -1,4 +1,41 @@
 package airline.ViewModels;
 
 public class FlightSearchViewModel {
+    String source;
+    String destination;
+    String flightNumber;
+    double fare;
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
 }
