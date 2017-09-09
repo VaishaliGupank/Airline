b3eaf48805ed807ea6cb676ccfe4229b585b779b
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-10 01:39:42 +0530

    Code Refactoring

--- src/main/java/airline/Models/Aeroplane.java
+++ src/main/java/airline/Models/Aeroplane.java
@@ -1,6 +1,7 @@
 package airline.Models;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Aeroplane {
 
@@ -18,6 +19,29 @@ public class Aeroplane {
 
     public List<TravelClass> getTravelClasses() {
         return travelClasses;
+    }
+
+    public int getNumberOfSeatsAvailable(TravelClass.TravelType travelType , int noOfSeatsBooked) {
+        Optional<TravelClass> travelClass = this.getTravelClasses().stream().
+                filter(plane -> plane.getTravelClass().equals(travelType)).findFirst();
+        if(travelClass.isPresent())
+        {
+            return (travelClass.get().getNoOfSeats() - noOfSeatsBooked);
+        }
+
+        return 0;
+    }
+
+    public double getBasefare(TravelClass.TravelType travelType)
+    {
+        Optional<TravelClass> travelClass = this.getTravelClasses().stream().
+                filter(plane -> plane.getTravelClass().equals(travelType)).findFirst();
+        if(travelClass.isPresent())
+        {
+            return (travelClass.get().getBasePrice());
+        }
+
+        return 0;
     }
 
 
