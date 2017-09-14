1a31e7369e78594bac99006fe0f6bd281fc1fa57
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-15 01:15:08 +0530

    [vaishali] Code Refactoring

--- src/main/java/airline/BAL/FirstClassPriceProcessor.java
+++ src/main/java/airline/BAL/FirstClassPriceProcessor.java
@@ -3,6 +3,7 @@ import airline.Utility.PricingXMLReader;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 public class FirstClassPriceProcessor extends PriceProcessor {
 
@@ -14,18 +15,22 @@ public class FirstClassPriceProcessor extends PriceProcessor {
 
     @Override
     public double getFare() {
-        /* Get the pricing rules for economy class */
-        float incrementPercentInFare = pricingXMLReader.getPricingRulesForFirstClass();
+        /* Get the pricing rules for first class */
+        float incrementPercentInFare = pricingXMLReader.getPercentIncrementInFareFirstClass();
         double totalFare = 0;
 
-        if(pricingModel.departureDate.isPresent())
-        {
-            LocalDate openingDateOfBookingFlight = pricingModel.departureDate.get().minusDays(10);
-            long daysBetweenDateOfTravelAndOpeningDateOfBookingFlight = ChronoUnit.DAYS.between(openingDateOfBookingFlight,
-                    LocalDate.now());
-            totalFare = Math.round(pricingModel.baseFare *
-                    Math.pow(incrementPercentInFare /100 + 1, daysBetweenDateOfTravelAndOpeningDateOfBookingFlight))
-                    * pricingModel.noOfRequestedSeats;
+        if(pricingModel.departureDate.isPresent()) {
+            Optional<Integer> noOfDaysBeforeBookingStarts =
+                    pricingXMLReader.getDaysWhenBookingIsAllowedBeforeDepartureDate("First");
+            if (noOfDaysBeforeBookingStarts.isPresent()) {
+                LocalDate openingDateOfBookingFlight = pricingModel.departureDate.get().
+                        minusDays(noOfDaysBeforeBookingStarts.get());
+                long daysBetweenDateOfTravelAndOpeningDateOfBookingFlight = ChronoUnit.DAYS.between(openingDateOfBookingFlight,
+                        LocalDate.now());
+                totalFare = Math.round(pricingModel.baseFare *
+                        Math.pow(incrementPercentInFare / 100 + 1, daysBetweenDateOfTravelAndOpeningDateOfBookingFlight))
+                        * pricingModel.noOfRequestedSeats;
+            }
         }
         else
             totalFare = pricingModel.baseFare * pricingModel.noOfRequestedSeats;
