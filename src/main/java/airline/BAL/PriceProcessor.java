b57b1a7f288bb60cea3a66e3786f818b3769d96f
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-15 12:09:11 +0530

    [vaishali] Added Validators Checking Logic

--- src/main/java/airline/BAL/PriceProcessor.java
+++ src/main/java/airline/BAL/PriceProcessor.java
@@ -2,14 +2,20 @@ package airline.BAL;
 
 import airline.Utility.PricingXMLReader;
 
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+
 public abstract class PriceProcessor implements IPriceProcessor {
 
     protected PricingModel pricingModel;
     protected PricingXMLReader pricingXMLReader;
+    protected static javax.validation.Validator validator;
     public PriceProcessor(PricingModel pricingModel,PricingXMLReader pricingXMLReader)
     {
         this.pricingModel = pricingModel;
         this.pricingXMLReader = pricingXMLReader;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 
     public double whatPercentFlightIsFull(int totalCapacity, int noOfBookedSeats)
